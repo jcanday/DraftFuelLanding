8356a2f5bf38e7c1b45f88339277c47b0de5ae20
jcanday <jc@milkstreetmarketing.com>
2021-04-23 14:32:58 -0400

    first files

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+var sheet = document.createElement('style'),  
+  $rangeInput = $('.range input'),
+  prefs = ['webkit-slider-runnable-track', 'moz-range-track', 'ms-track'];
+
+document.body.appendChild(sheet);
+
+var getTrackStyle = function (el) {  
+  var curVal = el.value,
+      val = (curVal - 1) * 33.333333,
+      style = '';
+  
+  // Set active label
+  $('.range-labels li').removeClass('active selected');
+  
+  var curLabel = $('.range-labels').find('li:nth-child(' + curVal + ')');
+  
+  curLabel.addClass('active selected');
+  curLabel.prevAll().addClass('selected');
+  
+  // Change background gradient
+  for (var i = 0; i < prefs.length; i++) {
+    style += '.range {background: linear-gradient(to right, #c5001a 0%, #c5001a ' + val + '%, #0E2B34 ' + val + '%, #0E2B34 100%)}';
+    style += '.range input::-' + prefs[i] + '{background: linear-gradient(to right, #c5001a 0%, #c5001a ' + val + '%, #0E2B34 ' + val + '%, #0E2B34 100%)}';
+  }
+
+  return style;
+}
+
+$rangeInput.on('input', function () {
+  sheet.textContent = getTrackStyle(this);
+});
+
+// Change input value on label click
+$('.range-labels li').on('click', function () {
+  var index = $(this).index();
+  
+  $rangeInput.val(index + 1).trigger('input');
+  
+});
+
+
